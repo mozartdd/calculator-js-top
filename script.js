4036eeebabfa4de33c98c005ff3b571112b3d6f3
Dainis Dilevka <mozart_dd@outlook.com>
2025-03-22 11:09:57 +0000

    started to work on math function, added html/css starting point

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+let a;
+let b;
+let operator;
+
+function operate(a, b, operator) {
+    if (operator === '+') {
+        return add(a, b);
+    };
+    if (operator === '-') {
+        return subtract(a, b);
+    };
+    if (operator === '*') {
+        return multiply(a, b);
+    };
+    if (operator === '/') {
+        return divide(a, b);
+    }
+};
+
+console.log(operate(2, 5, '-'));
+
